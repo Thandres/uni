05f4aad6416aebfcfb21df001a91748b967009bf
Arno Gabler <arno.gabler@hotmail.com>
2020-09-03 10:06:09 +0200

    Aufgabe 1

---
+++ JAPR01/src/main/java/Bruch.java
@@ -0,0 +1,46 @@
+public class Bruch {
+  private final int zaehler;
+  private final int nenner;
+
+  public Bruch(int zaehler, int nenner) {
+      if(nenner == 0){
+          throw new IllegalArgumentException("Nenner darf nicht 0 sein");
+      }
+    int teiler = ggt(zaehler, nenner);
+    this.zaehler = zaehler / teiler;
+    this.nenner = nenner / teiler;
+  }
+
+  public Bruch(int zaehler) {
+    this.zaehler = zaehler;
+    this.nenner = 1;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Zähler: %s, Nenner: %s", zaehler, nenner);
+  }
+
+  private int ggt(int a, int b) {
+    if (a == b) {
+      return a;
+    } else if (a < b) {
+      return ggt(a, b - a);
+    } else {
+      return ggt(b, a);
+    }
+  }
+
+  public Bruch add(Bruch other) {
+    int newZaehler = this.zaehler * other.nenner
+            + other.zaehler * this.nenner;
+    int newNenner = this.nenner * other.nenner;
+    return new Bruch(newZaehler, newNenner);
+  }
+
+  public Bruch multiply(Bruch other) {
+    return new Bruch(
+            this.zaehler * other.zaehler,
+            this.nenner * other.nenner);
+  }
+}
---
+++ JAPR01/src/test/java/BruchTest.java
@@ -0,0 +1,64 @@
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BruchTest {
+
+  @Nested
+  class Ein_Bruch {
+    @Nested
+    class hat_einen_Kontruktor_mit {
+
+      @Test
+      void zaehler_und_nenner_ohne_0_im_nenner() {
+        int i1 = 2;
+        int i2 = 4;
+
+        Bruch result = new Bruch(i1, i2);
+
+        assertThrows(IllegalArgumentException.class,()-> new Bruch(1,0));
+      }
+
+      @Test
+      void nur_zaehler() {
+        int zaehler = 4;
+
+        Bruch result = new Bruch(zaehler);
+      }
+    }
+
+    @Test
+    void kann_mit_anderen_bruechen_addiert_werden() {
+      var bruch1 = new Bruch(4);
+      var bruch2 = new Bruch(4, 3);
+
+      Bruch result = bruch1.add(bruch2);
+
+      assertNotNull(result);
+      assertEquals("Zähler: 16, Nenner: 3", result.toString());
+    }
+
+    @Test
+    void kann_mit_anderen_bruechen_multipliziert_werden() {
+      var bruch1 = new Bruch(8, 10);
+      var bruch2 = new Bruch(4, 3);
+
+      Bruch result = bruch1.multiply(bruch2);
+
+      assertNotNull(result);
+      assertEquals("Zähler: 16, Nenner: 15", result.toString());
+    }
+
+    @Test
+    void gibt_seinen_wert_mit_toString_vollstaendig_gekuerzt_aus() {
+      int i1 = 2;
+      int i2 = 4;
+      Bruch bruch = new Bruch(i1, i2);
+
+      String result = bruch.toString();
+
+      assertEquals("Zähler: 1, Nenner: 2", result);
+    }
+  }
+}
